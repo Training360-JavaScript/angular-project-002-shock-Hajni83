04ef6774519aaadd8be80ee32c4ce5a2826c58fe
Hajni83 <maro.hajnalka@gmail.com>
2022-01-16 16:08:06 +0100

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,31 @@ import { UserService } from './service/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'The good Angular programmer';
 
-users=this.userService.list;
+  users = this.userService.list;
 
-currentUser:User= new User();
+  currentUser: User = new User();
 
-  constructor(private userService:UserService) {}
+  constructor(private userService: UserService) {}
 
+  onSelectClick() {
+    // this.userService.
+    // this.currentUser = event.target;
+  }
+
+  onUpdateClick() {
+    this.userService.updateUser(this.currentUser);
+  }
+
+  onDeleteClick() {
+    this.userService.removeUser(this.currentUser);
+  }
+
+  delUser() {
+    this.userService.removeUser(this.currentUser);
+  }
 }
